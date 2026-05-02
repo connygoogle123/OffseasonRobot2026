96f8b930b04cb26d78660736fe4e15e1c9c8bb15
connygoogle123 <hellospirit28@gmail.com>
2026-05-01 21:16:34 -0400

    Add imports for Drivetrain and motor hardware

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/Tele.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/Tele.java
@@ -1,7 +1,12 @@
 package org.firstinspires.ftc.teamcode.opmodes.teleop;
+import org.firstinspires.ftc.teamcode.config.subsystems.Drivetrain; 
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 
 @TeleOp (name = "MainTeleOp", group = "TeleOp")
 public class Tele {
+
 }
